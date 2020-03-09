7b0a489c80d91005ca7aafa8ace0505fc96e5d45
LinusWKing <linuswahomeking@gmail.com>
2020-03-09 09:06:17 +0300

    heroku deployment changes

--- config.py
+++ config.py
@@ -20,3 +20,4 @@ class Config(object):
     ADMINS = os.environ.get('MAIL_USERNAME')
     LANGUAGES = ['en', 'es']
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
